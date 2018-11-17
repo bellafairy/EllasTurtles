63af23a98d7fc969104adbf6a9d6868bf3c9e858
BellaFairy <ella.marie.berg@gmail.com>
2018-11-17 11:22:50 -0600

    InitialCommit

---
+++ Turtles.py
@@ -0,0 +1,40 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor('maroon')
+
+bania_turtles = dict()
+bania_names = ['danelle', 'oliver', 'addy', 'patton', 'buck']
+bania_fg_colors = ['black', 'black', 'black', 'black', 'black']
+bania_bg_colors = ['red', 'orange', 'yellow', 'green', 'blue']
+
+for name in bania_names:
+    t = turtle.Turtle()
+    t.shape('turtle')
+    t.penup()
+    bania_turtles[name] = t
+
+
+for index, name in enumerate(bania_names):
+    bania_turtles[name].color(
+        bania_fg_colors[index],
+        bania_bg_colors[index])
+for y in {0, -100}:
+    for x in {0, -100}:
+        print([x, y])
+bania_turtles['buck'].setposition(-50, 100)
+bania_turtles['patton'].setposition(0, 0)
+bania_turtles['addy'].setposition(-100, 0)
+bania_turtles['oliver'].setposition(0, -100)
+bania_turtles['danelle'].setposition(-100, -100)
+
+for t in bania_turtles.values():
+    t.pendown()
+    t.begin_fill()
+    for number in range(10):
+        t.forward(50)
+        t.left(150)
+        t.forward(50)
+        t.right(150)
+    t.end_fill()
+screen.exitonclick()
+screen.mainloop()
